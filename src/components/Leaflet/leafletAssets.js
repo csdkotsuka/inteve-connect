a6fd69c28496d31a96d300cfe574793d173dc800
csdkotsuka <csdkotsuka@creativesd.net>
2026-09-07 16:17:02 +0900

    fix: resolve build error by removing unneeded raw screenshot imports

--- src/components/Leaflet/leafletAssets.js
+++ src/components/Leaflet/leafletAssets.js
@@ -1,15 +1,3 @@
-// Leaflet assets helper
-import shotAuthModal from '../../../screen shot/Screenshot 2026-09-05 at 10.07.31.png';
-import shotAiChat from '../../../screen shot/Screenshot 2026-09-05 at 10.07.40.png';
-import shotSlotSelect from '../../../screen shot/Screenshot 2026-09-05 at 10.07.54.png';
-import shotTimeline from '../../../screen shot/Screenshot 2026-09-05 at 10.08.01.png';
-import shotCustomerList from '../../../screen shot/Screenshot 2026-09-05 at 10.08.07.png';
-import shotMessaging from '../../../screen shot/Screenshot 2026-09-05 at 10.08.13.png';
-import shotAnnouncement from '../../../screen shot/Screenshot 2026-09-05 at 10.08.16.png';
-import shotSchedule from '../../../screen shot/Screenshot 2026-09-05 at 10.08.19.png';
-import shotServices from '../../../screen shot/Screenshot 2026-09-05 at 10.08.30.png';
-import shotStaffCalendar from '../../../screen shot/Screenshot 2026-09-05 at 10.08.33.png';
-
 // Cropped high-clarity focal screenshots
 import cropAuthModal from './cropped/step1_auth_modal.png';
 import cropAiChat from './cropped/step2_ai_chat.png';
@@ -21,16 +9,16 @@ import cropSchedule from './cropped/key4_schedule_crop.png';
 import cropConnectQr from './cropped/connect_qr.png';
 
 export const SCREENSHOTS = {
-  authModal: shotAuthModal,
-  aiChat: shotAiChat,
-  slotSelect: shotSlotSelect,
-  timeline: shotTimeline,
-  customerList: shotCustomerList,
-  messaging: shotMessaging,
-  announcement: shotAnnouncement,
-  schedule: shotSchedule,
-  services: shotServices,
-  staffCalendar: shotStaffCalendar,
+  authModal: cropAuthModal,
+  aiChat: cropAiChat,
+  slotSelect: cropSlotSelect,
+  timeline: cropTimeline,
+  customerList: cropCustomer,
+  messaging: cropAiChat,
+  announcement: cropTimeline,
+  schedule: cropSchedule,
+  services: cropSlotSelect,
+  staffCalendar: cropStaffCalendar,
 };
 
 export const CROPPED = {
